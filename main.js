3680d9c52c1de0b107a33dce559adf69dfdf4e16
samuerikcoder <eriksamumedeiros@gmail.com>
2023-06-16 16:07:52 -0300

    new commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let picture = document.querySelector('.order-summary__img');
+let headphone_icon = document.querySelector('.fa-headphones');
+
+picture.addEventListener('mousemove', (event) => {
+    const x = event.clientX - picture.offsetLeft;
+    const y = event.clientY - picture.offsetTop;
+    
+    headphone_icon.style.left = `${x}px`;
+    headphone_icon.style.top = `${y}px`;
+});
