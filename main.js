107606fe32526e67aa9ddacd64d23acc0e154e67
samuerikcoder <eriksamumedeiros@gmail.com>
2023-06-16 16:12:20 -0300

    new commit

--- main.js
+++ main.js
@@ -8,3 +8,11 @@ picture.addEventListener('mousemove', (event) => {
     headphone_icon.style.left = `${x}px`;
     headphone_icon.style.top = `${y}px`;
 });
+
+picture.addEventListener('touchmove', (event) => {
+    const x = event.touches[0].clientX - picture.offsetLeft;
+    const y = event.touches[0].clientY - picture.offsetTop;
+    
+    headphone_icon.style.left = `${x}px`;
+    headphone_icon.style.top = `${y}px`;
+  });
